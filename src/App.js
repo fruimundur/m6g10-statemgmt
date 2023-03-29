77bc7034cfe48bc7fed6ec8a61d8dcb9056a36b6
Fríðmundur Nattestad <fnattestad@gmail.com>
2023-03-29 10:47:43 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { useRecoilState } from 'recoil'
+import { countState } from "./atoms"
 
 function App() {
+  // const [count, setCount] = useState(0)
+  const [count, setCount] = useRecoilState(countState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className="Heading1">Counter</h1>
+      <h2>{count}</h2>
+      <div className="ButtonContainer">
+        <button className="IncButton" onClick={() => setCount(count + 1)}>
+          Increment
+        </button>
+        <button className="DecButton" onClick={() => setCount(count - 1)} disabled={count === 0}>
+          Decrement
+        </button>
+        <button className="ResButton" onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
